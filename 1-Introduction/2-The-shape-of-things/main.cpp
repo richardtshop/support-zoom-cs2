6978a0c24ec552eb055d1a21cc78cd17bc53263c
Rich T <richard.todd@shopify.com>
2020-09-07 20:26:40 -0400

    Shape of things 2

--- 1-Introduction/2-The-shape-of-things/main.cpp
+++ 1-Introduction/2-The-shape-of-things/main.cpp
@@ -1,38 +1,57 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    int row = 1;
-    int columns = 19;
-    int midRow = 5; // calculate the relationship between the midrow and the and the columns
+    int currentRow = 1;      // starting row
+    int centerRowIndex = 5;  // last row before image is mirrored
+    int totalColumns = 19;   // total columns
 
-    bool reverse = false;
-    while (row >= 1 && row <= midRow) {
-        for (int hashNum = 1; hashNum <= columns; hashNum++) {
+    // The loop will count up to the centerRow and then reverse counting down
+    // to produce the mirrored printout
+    // This boolean will be used to check if the row numbers in the loop should increase or decrease
+    bool rowNumberIncreasing = true;
+
+    // loop while the rows are between 1 and the center (halfway row)
+    // as the loop is counting up then down, this will create all the rows we need
+    while (currentRow >= 1 && currentRow <= centerRowIndex) {
+        // nested for loop going through each column position in each row from left to right
+        for (int currentColumn = 1; currentColumn <= totalColumns; currentColumn++) {
+            // the three conditions in the if statement calculate if a # should be printed
+            // 1. Calculate the right-hand start and end for printing # for each row
+            // Start printing: current column is larger/equal to the current row
+            // End printing: current column is smaller then 2* row
+
+            // 2. Calculate the right-hand start and end for printing # for each row
+            // Start printing: current column smaller than total columns minus current row + 1
+            // End printing: current column is greater than total columns minus ((current row - 1) * 2)
+
+            // 3. The above doesn't calculate for the center row, so if the loop is on the center row, also
+            // print if the current column is double the center row value
             if (
-                (hashNum >= row && hashNum < row + row) ||
-                (hashNum <= columns - row + 1 && hashNum >= columns - (row - 1) * 2) ||
-                (row == midRow && hashNum == midRow * 2)) {
+                (currentColumn >= currentRow && currentColumn < 2 * currentRow) ||
+                (currentColumn <= totalColumns - currentRow + 1 && currentColumn >= totalColumns - ((currentRow - 1) * 2)) ||
+                (currentRow == centerRowIndex && currentColumn == centerRowIndex * 2)) {
                 cout << '#';
-            }
-            else {
+            } else {
                 cout << ' ';
             }
         }
+
         cout << '\n';
-        if (row == midRow && reverse == false) {
-            reverse = true;
-            row++;
+        // When we reach the center row index in the loop when incrementing, set the rowNumberIncreasing to false to reverse the loop
+        if (currentRow == centerRowIndex && rowNumberIncreasing == true) {
+            rowNumberIncreasing = false;
+            currentRow++;  // increment once more so the below if statement will correctly decrement and repeat the center row
         }
 
-        if (reverse) row--;
-        else row++;
+        // If rowNumberIncreasing then we want to incremement the currenRow, otherwise decrement
+        if (rowNumberIncreasing) {
+            currentRow++;
+        } else {
+            currentRow--;
+        }
     }
     return 0;
 }
-
-// hashnum == change variable name
-// comment
-// extract the calculations in the for loop and make it more obvious
